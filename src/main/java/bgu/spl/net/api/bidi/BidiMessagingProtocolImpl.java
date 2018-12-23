64c79dee811f631f7bc445517a3a6a19761a5585
noafeiger <noafieger29@gmail.com>
2018-12-23 21:29:24 +0200

    noa

--- src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -2,11 +2,15 @@ package bgu.spl.net.api.bidi;
 
 public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<T> {
 
+    private  int con_id;
+    private Connections connections;
+
     public BidiMessagingProtocolImpl() {
     }
 
     public void start(int connectionId, Connections<T> connections) {
-
+        this.con_id=connectionId;
+        this.connections=connections;
     }
 
     public void process(T message) {
--- src/main/java/bgu/spl/net/srv/bidi/ConnectionHandlerImpl.java
+++ src/main/java/bgu/spl/net/srv/bidi/ConnectionHandlerImpl.java
@@ -27,6 +27,31 @@ public class ConnectionHandlerImpl<T> implements ConnectionHandler<T> {
     public void send(T msg) {
     }
 
+    public void run() {
+        try (Socket sock = this.sock) { //just for automatic closing
+            int read;
+
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                }
+                /*
+                if (response != null) {
+                    out.write(encdec.encode(response));
+                    out.flush();
+                }
+             */
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
     @Override
     public void close() throws IOException {
         connected = false;

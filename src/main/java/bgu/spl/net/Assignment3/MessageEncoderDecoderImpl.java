bc0c7f84ba50f83103ab5fc31c0fa290601a798f
noafeiger <noafieger29@gmail.com>
2018-12-25 21:08:32 +0200

    Merge branch 'master' of https://github.com/Ohad1/spl-net
    # Conflicts:
    #	src/main/java/bgu/spl/net/api/MessageEncoderDecoderImpl.java

--- src/main/java/bgu/spl/net/Assignment3/MessageEncoderDecoderImpl.java
+++ src/main/java/bgu/spl/net/Assignment3/MessageEncoderDecoderImpl.java
@@ -191,10 +191,29 @@ public class MessageEncoderDecoderImpl implements MessageEncoderDecoder<String> 
                 byte[]opcode_request_byte=shortToBytes(Short.parseShort(split[1]));
                return DefaultConvert(type_byte,opcode_request_byte);
             }
+            case "9": {
+                byte[]type_byte=shortToBytes((short) 9); //NOTIFICATION
+                return Notification(split,message,type_byte);
+            }
         }
         return null;
     }
 
+    private byte[] Notification(String[]split,String line, byte[] type_byte) {
+
+        byte[]pm_post=(split[1]).getBytes();//TODO CHECK CHAR
+        byte[]posting_user=(split[2]+"\0").getBytes();
+        String s="";
+        for(int i=3;i<split.length;i++){
+            s=s.concat(split[i]+" ");
+        }
+        byte[]content=(s+"\0").getBytes();
+
+        byte[]opcode_char=DefaultConvert(type_byte,pm_post);
+        byte[]opcode_char_postuser=DefaultConvert(opcode_char,posting_user);
+        byte[]fin=DefaultConvert(opcode_char_postuser,content);
+        return fin;
+    }
 
     private void pushByte(byte nextByte) {
         if (len >= bytes.length) {
